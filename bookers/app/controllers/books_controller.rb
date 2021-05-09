5236b79a4357c9bddb4d1ad937f723c1c11fa5e5
MasakoIdei <tarte.groseilles@gmail.com>
2021-05-09 20:54:22 +0000

    [add]show画面へリンク

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -2,17 +2,38 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @post = Book.new
-    
+
   end
 
   def create
-    book = Book.new
+    book = Book.new(book_params)
     book.save
+    redirect_to books_path(book.id)
   end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+
+
+private
+def book_params
+   params.require(:book).permit(:title, :body)
+
+end
+
 end
