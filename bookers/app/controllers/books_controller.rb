cf616b1fd3dc6689c38d2e36e14639dcc42e2383
MasakoIdei <tarte.groseilles@gmail.com>
2021-05-10 08:41:38 +0000

    [fix]削除の確認popの未表示を修正

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,52 +1,54 @@
 class BooksController < ApplicationController
-  def index
-    @books = Book.all
-    @post = Book.new
+   def index
+     @books = Book.all
+     @post = Book.new
+   end
 
-  end
+   def create
+    　book = Book.new(book_params)
+       if book.save
+       　flash[:notice] ="Book was succesfull created."
+      　 redirect_to book_path(book.id)
+       else
+      　 render :new
+    　 end
+   end
 
-  def create
-    book = Book.new(book_params)
-    if book.save
-      flash[:notice] ="Book was succesfull created."
-    end
-    redirect_to book_path(book.id)
+   def show
+     @book = Book.find(params[:id])
+   end
 
-  end
+   def edit
+     @book = Book.find(params[:id])
+   end
 
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    book = Book.find(params[:id])
-    
-    if book.update(book_params)
-    flash[:notice] ="Book was succesfull up."
-    end
-    redirect_to book_path(book.id)
+   def update
+     book = Book.find(params[:id])
+     if book.update(book_params)
+      flash[:notice] ="Book was succesfull up."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+      end
   end
 
   def destroy
     book = Book.find(params[:id])
     if book.destroy
        flash[:notice] ="Book was succesfull destroy."
+       redirect_to books_path
     end
-      
-    redirect_to books_path
-  end
+
+
+
+end
 
 
 
 
 private
-def book_params
+  def book_params
    params.require(:book).permit(:title, :body)
-
-end
+  end
 
 end
